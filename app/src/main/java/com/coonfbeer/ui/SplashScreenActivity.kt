266fc4339edfaf63f38ed8fd59e002915d72691d
Matias Acevedo <matiasacevedoivan@gmail.com>
2020-09-03 20:53:33 -0300

    Initial commit

---
+++ app/src/main/java/com/coonfbeer/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.coonfbeer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.coonfbeer.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
